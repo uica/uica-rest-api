7abc9d8e147e3552842832f545f871c034676d68
Muhammad Al Juburi <maljuburi89@gmail.com>
2020-05-10 17:22:46 -0700

    changed index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
+const { version } = require("../package.json");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", {
+    title: "UICA",
+    version,
+  });
 });
 
 module.exports = router;
